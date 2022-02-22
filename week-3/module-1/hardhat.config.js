a370a8d44534264948064c7c9a58fe33a8810a30
mounicasruthi <mounicasruthi30@gmail.com>
2022-02-23 03:31:41 +0530

    deployed to a live network

--- week-3/module-1/hardhat.config.js
+++ week-3/module-1/hardhat.config.js
@@ -1,14 +1,15 @@
+// const { hardhatArguments } = require("hardhat/config");
+
+require("@nomiclabs/hardhat-ethers");
 require("@nomiclabs/hardhat-waffle");
 
-// Go to https://www.alchemyapi.io, sign up, create
-// a new App in its dashboard, and replace "KEY" with its key
-const alchemy_api_key = process.env.ALCHEMY_API_KEY;
+// require("dotenv").config({ path: "module-1/.env" });
+require("dotenv").config();
 
-// Replace this private key with your Ropsten account private key
-// To export your private key from Metamask, open Metamask and
-// go to Account Details > Export Private Key
-// Be aware of NEVER putting real Ether into testing accounts
+const alchemy_api_key = process.env.ALCHEMY_API_KEY;
 const ropsten_private_key = process.env.ROPSTEN_PRIVATE_KEY;
+
+// console.log("hi", alchemy_api_key, ropsten_private_key);
 
 module.exports = {
   solidity: "0.7.3",
@@ -19,3 +20,18 @@ module.exports = {
     }
   }
 };
+
+// Go to https://www.alchemyapi.io, sign up, create
+// a new App in its dashboard, and replace "KEY" with its key
+
+// Replace this private key with your Ropsten account private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Be aware of NEVER putting real Ether into testing accounts
+
+// hardhatArguments.config({
+//   alchemy_api_key: process.env.ALCHEMY_API_KEY,
+// ropsten_private_key: process.env.ROPSTEN_PRIVATE_KEY
+// })
+
+// const { ALCHEMY_API_KEY, ROPSTEN_PRIVATE_KEY } = process.env;
